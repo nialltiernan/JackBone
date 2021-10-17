3253e0b2bfc67c0ccd4ae97489358ebf3ed643aa
Niall Tiernan <nialltiernan93@gmail.com>
2021-10-17 16:33:23 +0200

    Initial commit for user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\Type\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'users.index')]
+    public function index(UserRepository $repository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/users/create', name: 'users.create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, new User());
+        $form->handleRequest($request);
+
+        if ($this->shouldCreateUser($form)) {
+            /** @var User $user */
+            $user = $form->getData();
+            $user->setCreatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->renderForm('user/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/users/{user}', name: 'users.show')]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    private function shouldCreateUser(FormInterface $form): bool
+    {
+        return $form->isSubmitted() && $form->isValid();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
